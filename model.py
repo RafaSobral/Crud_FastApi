ea51ff057b2403d255029fa4f9ec692d164696a5
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2026-08-11 19:38:32 -0300

    FIX adicionando model correta, tinha subido arquivo errado quando renomeei

--- model.py
+++ model.py
@@ -1,30 +1,9 @@
-import sqlite3 
-
-DATABASE_NAME = "bancoAlunos.db"
-
-def conexao_banco():
-    conectar = sqlite3.connect(DATABASE_NAME)
-    conectar.row_factory = sqlite3.Row
-    return conectar
-
-def criar_tabela_alunos():
-    conectar = conexao_banco()
-
-    conectar.execute("""
-        CREATE TABLE IF NOT EXISTS usuarios (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
-            idade INTEGER NOT NULL,
-            curso TEXT NOT NULL,
-            periodo INTEGER NOT NULL
-        ) 
-""")
-
-    conectar.commit()
-    conectar.close()
+class Aluno:
+    def __init__(self, nome, idade, curso, periodo, id=None):
+        self.nome = nome
+        self.idade = idade
+        self.curso = curso
+        self.periodo = periodo
+        self.id = id
 
 
-
-
-
-    
